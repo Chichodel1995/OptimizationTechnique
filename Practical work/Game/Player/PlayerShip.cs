ff286205921b39c8c4a472a25aeee55e1c1ff8b7
Chichodel1995 <>
2020-06-02 01:06:59 -0300

    Pequeña optimización
    En el código comentado está rotando a cada sprite.
    La solución fue quitarlo y reemplazar por el spirte desea guardandolo en una variable de tipo Image.

--- Practical work/Game/Player/PlayerShip.cs
+++ Practical work/Game/Player/PlayerShip.cs
@@ -183,11 +183,12 @@ namespace Game
         private Image LoadImage()
         {
             Image[] ships = Spritesheet.Load(@"Resources\shipsheetparts.png", new Size(200, 200));
-            foreach (Image img in ships)
-            {
-                img.RotateFlip(RotateFlipType.Rotate270FlipNone);
-            }
+            //foreach (Image img in ships)
+           // {
+                //img.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            //}
             Image result = ships[shipIndex];
+            result.RotateFlip(RotateFlipType.Rotate270FlipNone);
             result.RotateFlip(RotateFlipType.RotateNoneFlipX);
             Extent = new SizeF(result.Size.Width / 2, result.Size.Height / 2);
             return result;
